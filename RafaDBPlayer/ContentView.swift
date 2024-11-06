8e62019b8bbb2bce96c1c1890b95b66e682bce4b
rafael <rloggiodice@eservicios.indra.es>
2024-11-06 13:34:14 +0100

    Initial Commit

---
+++ RafaDBPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RafaDBPlayer
+//
+//  Created by Rafael Loggiodice on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RafaDBPlayer/RafaDBPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafaDBPlayerApp.swift
+//  RafaDBPlayer
+//
+//  Created by Rafael Loggiodice on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RafaDBPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
