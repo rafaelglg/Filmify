2fee179c264c673dd9b829d1d6ff1c0addcdb68f
rafael <rloggiodice@eservicios.indra.es>
2024-11-15 16:34:08 +0100

    [ Clean ] organice folders, starting to create CastSectionView

---
+++ RafaDBPlayer/Presentation/View/DashboardView/DetailView/SectionsDetailView/CastSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CastSectionView.swift
+//  RafaDBPlayer
+//
+//  Created by Rafael Loggiodice on 15/11/24.
+//
+
+import SwiftUI
+
+struct CastSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CastSectionView()
+}
