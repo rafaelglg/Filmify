581b9e8bf72a6553328fa6796ead1d5bc16a6efd
rafael <rloggiodice@eservicios.indra.es>
2024-12-29 11:29:07 +0100

    [feature] add localizations, re ident code and change name to filmify

--- Filmify/UI/FilmifyApp.swift
+++ Filmify/UI/FilmifyApp.swift
@@ -1,5 +1,5 @@
 //
-//  Filmify.swift
+//  FilmifyApp.swift
 //  Filmify
 //
 //  Created by Rafael Loggiodice on 6/11/24.
@@ -9,16 +9,16 @@ import SwiftUI
 import FirebaseCore
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 @main
-struct RafaDBPlayerApp: App {
+struct FilmifyApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
